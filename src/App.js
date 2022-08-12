9ebcfdfa8612b5ea77c85a63b2afb1ceba0c5556
harshsoniaimdek <harsh.soni@aimdek.com>
2022-08-12 17:56:16 +0530

    added css and Routing

--- src/App.js
+++ src/App.js
@@ -1,18 +1,80 @@
+import logo from "./logo.svg";
+import "./App.css";
+import Homepage from "./pages/Homepage";
+import { Route, BrowserRouter, Routes, Navigate } from "react-router-dom";
+import Dishes from "./pages/Dishes";
+import CartPage from "./pages/CartPage";
+import LoginPage from "./pages/LoginPage";
+import RegisterPage from "./pages/RegisterPage";
 
-import './App.css';
-import Homepage from './Pages/HomePage';
-import CartPage from './Pages/CartPage';
-import LoginPage from './Pages/LoginPage';
-import RegisterPage from './Pages/RegisterPage';
+import "./stylesheets/layout.css";
+import "./stylesheets/products.css";
+import "./stylesheets/authentication.css";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
+import AdminPage from "./pages/AdminPage";
+
 function App() {
   return (
     <div className="App">
-      <Homepage />
-      {/* <CartPage /> */}
-      {/* <LoginPage /> */}
-      {/* <RegisterPage /> */}
+      <ToastContainer />
+      <BrowserRouter>
+        <Routes>
+          <Route
+            path="/"
+            exact
+            element={
+              <ProtectedRoutes>
+                <Homepage />
+              </ProtectedRoutes>
+            }
+          />
+          <Route
+            path="/dish"
+            exact
+            element={
+              <ProtectedRoutes>
+                <Dishes />
+              </ProtectedRoutes>
+            }
+          />
+          
+         
+          <Route
+            path="/cart"
+            exact
+            element={
+              <ProtectedRoutes>
+                <CartPage />
+              </ProtectedRoutes>
+            }
+          />
+           
+           <Route
+            path="/admin"
+            exact
+            element={
+              <ProtectedRoutes>
+                <AdminPage />
+              </ProtectedRoutes>
+            }
+          />
+
+          <Route path="/login" exact element={<LoginPage />} />
+          <Route path="/register" exact element={<RegisterPage />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
 
 export default App;
+
+export const ProtectedRoutes = ({ children }) => {
+  if (localStorage.getItem("currentUser")) {
+    return children;
+  } else {
+    return <Navigate to="/login" />;
+  }
+};
